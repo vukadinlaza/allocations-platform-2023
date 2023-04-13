d02c0bd7234afc025fa831188394b2bc654c7651
Taylor Coffelt <taylorcoffelt@gmail.com>
2023-04-13 11:31:48 -0600

    fix(auth): fixes authentication for login

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['avatars.githubusercontent.com', 'avatar.vercel.sh']
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
   },
   experimental: {
     appDir: true,
--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -4,11 +4,12 @@ import Auth0Provider from 'next-auth/providers/auth0';
 export const authOptions: NextAuthOptions = {
   providers: [
     Auth0Provider({
-      clientId: process.env.AUTH0_CLIENT_ID as string,
-      clientSecret: process.env.AUTH0_CLIENT_SECRET as string,
+      clientId: process.env.AUTH0_CLIENT as string,
+      clientSecret: process.env.AUTH0_SECRET as string,
       issuer: process.env.AUTH0_ISSUER as string,
     }),
   ],
+  debug: true
 };
 
 export default NextAuth(authOptions);
