40b220589870ebd0c3fed577d4aac1ca4007e6d4
Taylor Coffelt <taylorcoffelt@gmail.com>
2023-04-14 14:43:16 -0600

    fix(auth): Fixes signout to force re-auth on login

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,8 +1,7 @@
 import NextAuth, { NextAuthOptions, Session } from 'next-auth';
 import Auth0Provider from 'next-auth/providers/auth0';
 import { SupabaseAdapter } from '@next-auth/supabase-adapter';
-import jwt from "jsonwebtoken"
-
+import jwt from 'jsonwebtoken';
 
 export const authOptions: NextAuthOptions = {
   providers: [
@@ -10,27 +9,32 @@ export const authOptions: NextAuthOptions = {
       clientId: process.env.AUTH0_CLIENT as string,
       clientSecret: process.env.AUTH0_SECRET as string,
       issuer: process.env.AUTH0_ISSUER as string,
-    }),
+      authorization: {
+        params: {
+          prompt: 'login'
+        }
+      }
+    })
   ],
   adapter: SupabaseAdapter({
     url: process.env.SUPABASE_URL as string,
-    secret: process.env.SUPABASE_KEY as string,
+    secret: process.env.SUPABASE_KEY as string
   }),
   callbacks: {
     async session({ session, user }) {
-      const signingSecret = process.env.SUPABASE_JWT_SECRET
+      const signingSecret = process.env.SUPABASE_JWT_SECRET;
       if (signingSecret) {
         const payload = {
-          aud: "authenticated",
+          aud: 'authenticated',
           exp: Math.floor(new Date(session.expires).getTime() / 1000),
           sub: user.id,
           email: user.email,
-          role: "authenticated",
-        }
-        session.supabaseAccessToken = jwt.sign(payload, signingSecret)
+          role: 'authenticated'
+        };
+        session.supabaseAccessToken = jwt.sign(payload, signingSecret);
       }
-      return session
-    },
+      return session;
+    }
   },
   debug: true
 };
