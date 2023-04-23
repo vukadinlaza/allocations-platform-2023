60b3631cf47bfac610fca787fb4c12ddf5ba32b5
Kingsley <kadvani1@gmail.com>
2023-04-22 21:08:16 -0400

    added rocketship

--- components/None.tsx
+++ components/None.tsx
@@ -6,7 +6,7 @@ import Image from 'next/image';
 export default function None({ text }) {
   return (
     <Card className="card" variant="outlined">
-      <Image src="/empty_target.svg" className="mb-4" width={75} height={75} />
+      <Image src="/astronomy-launch-rocket-2-svgrepo-com.svg" className="mb-4" width={75} height={75} />
       <p>{text || ''}</p>
     </Card>
   );
