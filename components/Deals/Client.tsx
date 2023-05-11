50b491ff90c72274852560ad388ed53ac5fba6b2
Guillaume Duhan <31253241+guillaumeduhan@users.noreply.github.com>
2023-05-11 18:29:39 +0100

    commit

---
+++ components/Deals/Client.tsx
@@ -0,0 +1,7 @@
+'use client';
+
+import { useState } from 'react';
+export default function DealClient() {
+  const [loading, setLoading] = useState<boolean>(true);
+  return <div>hello</div>;
+}
--- components/Loading/Deal.tsx
+++ components/Loading/Deal.tsx
@@ -2,19 +2,30 @@
 
 export default function LoadingDeal() {
   return (
-    <header className="grid grid-cols-4 gap-4 h-96">
-      <div className="col-span-2 loading"></div>
-      <div className="grid grid-cols-2 col-span-2 gap-4">
+    <div className="container mb-12">
+      <header className="flex items-center justify-between my-8">
+        <div className="flex items-center gap-4">
+          <div className="h-12 w-72 loading" />
+          <div className="w-32 h-8 rounded-full loading" />
+        </div>
+        <div className="w-32 h-10 loading" />
+      </header>
+      <main>
+        <div className="w-full mb-8 loading" style={{ height: '500px' }} />
+        <div className="grid grid-cols-5 gap-8 mb-8">
+          <div className="h-32 loading" />
+          <div className="h-32 loading" />
+          <div className="h-32 loading" />
+          <div className="h-32 loading" />
+          <div className="h-32 loading" />
+        </div>
         <div className="flex flex-col gap-4">
-          <div className="h-12 loading"></div>
-          <div className="w-64 h-12 loading"></div>
-          <div className="w-48 h-12 loading"></div>
+          <div className="w-full h-12 loading"></div>
+          <div className="w-full h-8 loading"></div>
+          <div className="w-full h-64 loading"></div>
+          <div className="w-full h-8 loading"></div>
         </div>
-        <div className="flex flex-col items-end gap-4">
-          <div className="w-64 h-20 loading"></div>
-          <div className="w-64 h-20 loading"></div>
-        </div>
-      </div>
-    </header>
+      </main>
+    </div>
   );
 }
