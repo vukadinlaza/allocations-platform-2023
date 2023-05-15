e7417d7112afef6eddd7b360d9b6647500326cfc
Guillaume Duhan <31253241+guillaumeduhan@users.noreply.github.com>
2023-05-15 11:13:58 +0100

    commit

--- components/Login.tsx
+++ components/Login.tsx
@@ -1,9 +1,11 @@
 'use client';
+/* eslint-disable react/no-unescaped-entities */
 import { useSupabase } from '@/lib/supabase-provider';
-import { Alert, CircularProgress } from '@mui/material';
+import { Alert } from '@mui/material';
+import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
-import Logo from './Logo';
+import Button from './Button';
 
 interface EmailStatus {
   type: 'success' | 'error';
@@ -61,46 +63,70 @@ export default function Login() {
   };
 
   return (
-    <div
-      className="p-8 mx-auto my-6 bg-white border rounded-lg"
-      style={{ width: '400px' }}
-    >
-      <div className="mb-4">
-        <Logo />
-      </div>
-      <p className="mb-4">
-        Welcome to Allocations v2.0.
-        <br />
-        Please enter your e-mail to login.
-      </p>
-      <form
-        onSubmit={async (e) => {
-          e.preventDefault();
-          await login();
-        }}
-      >
-        {!status && (
-          <div>
-            <input
-              type="text"
-              id="outlined-basic"
-              placeholder="mail@address.com"
-              className="w-full mb-4"
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </div>
-        )}
-        {status && <Alert severity={status.type}>{status.message}</Alert>}
-        {!status && (
-          <button
-            onClick={login}
-            className={`btn primary ${loading ? 'loading' : ''}`}
+    <div className="grid h-screen grid-cols-1 lg:grid-cols-5">
+      <div className="flex items-center justify-center p-6 bg-white md:p-8 md:col-span-2">
+        <div className="w-full lg:pb-24">
+          <header className="mb-6">
+            <div className="mb-8 md:mb-16 lg:mb-20">
+              <Image
+                src="/logo.png"
+                alt="Allocations.com"
+                width={200}
+                height={30}
+                className="cursor-pointer"
+                onClick={() => router.push('/')}
+              />
+            </div>
+            <h2 className="mb-2 text-xl">Welcome back.</h2>
+            <p>Sign in to your account.</p>
+          </header>
+          <form
+            onSubmit={async (e) => {
+              e.preventDefault();
+              await login();
+            }}
           >
-            {loading && <CircularProgress color="inherit" size={12} />}
-            {!loading && 'Sign in'}
-          </button>
-        )}
-      </form>
+            {!status && (
+              <div>
+                <input
+                  type="text"
+                  id="outlined-basic"
+                  placeholder="mail@address.com"
+                  className="w-full mb-4"
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+            )}
+            {status && <Alert severity={status.type}>{status.message}</Alert>}
+            {!status && (
+              <div className="grid">
+                <Button
+                  color="primary btn--big"
+                  onClick={login}
+                  loading={loading}
+                  label="Sign in"
+                />
+              </div>
+            )}
+          </form>
+        </div>
+      </div>
+      <div className="p-6 text-white md:col-span-3 bg-primary-500 md:p-8 lg:p-12 bg-1">
+        <div style={{ maxWidth: 400 }}>
+          <h2
+            className="mb-4 text-xl md:text-2xl lg:text-4xl"
+            style={{ lineHeight: 1.45 }}
+          >
+            Discover What's New in Allocations 2.0
+          </h2>
+          <p className="text-base text-white lg:text-lg">
+            With Allocations v2.0, users can expect a more streamlined and
+            intuitive experience. We have added a range of exciting nex features
+            that will help users to manage their investments with greater ease
+            and efficiency.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
