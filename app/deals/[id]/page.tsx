77cde9e58584975860b2f04811fbe6b2520bc365
Guillaume Duhan <31253241+guillaumeduhan@users.noreply.github.com>
2023-05-22 13:56:29 +0100

    commit

--- app/deals/[id]/page.tsx
+++ app/deals/[id]/page.tsx
@@ -13,39 +13,39 @@ export default function DealID() {
   const { supabase } = useSupabase();
   const { user } = useAuthContext();
   const [deal, setDeal] = useState<any>(null);
+  const [isAdmin, setIsAdmin] = useState<any>(null);
   const [loading, setLoading] = useState<boolean>(false);
   const params = useParams();
 
-  const isDealOwner = () => deal && user && deal.user_email === user.email; // fund manager deal.user_email === user.email
+  async function fetchDeal() {
+    if (!params || !params.id) return;
+    try {
+      setLoading(true);
+      // isAdmin? private_deals : public_deals
+      const { data: _deal, error } = await supabase
+        .from('deals')
+        .select('*')
+        .eq('id', params.id)
+        .single();
+
+      if (error) {
+        throw error;
+      }
+
+      if (_deal) {
+        console.log(_deal);
+        setDeal(_deal);
+      }
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  }
 
   useEffect(() => {
-    async function fetchDeal() {
-      if (!params || !params.id) return;
-      try {
-        setLoading(true);
-        // isDealOwner? private_deals : public_deals
-        const { data: _deal, error } = await supabase
-          .from('deals')
-          .select('*')
-          .eq('id', params.id)
-          .single();
-
-        if (error) {
-          throw error;
-        }
-
-        if (_deal) {
-          console.log(_deal);
-          setDeal(_deal);
-        }
-      } catch (error) {
-        console.error(error);
-      } finally {
-        setLoading(false);
-      }
-    }
-
     fetchDeal();
+    setIsAdmin(deal && user && deal.user_email === user.email);
   }, []);
 
   return (
@@ -54,8 +54,8 @@ export default function DealID() {
       {!loading && !deal && <None text="No deal found." />}
       {!loading && deal && (
         <div>
-          {isDealOwner() && <AdminDeal deal={deal} />}
-          {!isDealOwner() && <ClientDeal deal={deal} />}
+          {isAdmin && <AdminDeal deal={deal} />}
+          {!isAdmin && <ClientDeal deal={deal} />}
         </div>
       )}
     </div>
