b0e068579bebdc9b46a7642bb184cc10ed6ad752
Taylor Coffelt <taylorcoffelt@gmail.com>
2023-06-06 11:05:25 -0600

    fix window reference

--- app/(public)/verify/page.tsx
+++ app/(public)/verify/page.tsx
@@ -6,7 +6,7 @@ export default function Verify({ searchParams }: { searchParams: any }) {
   const router = useRouter();
 
   useEffect(() => {
-    if(window && searchParams.token) {
+    if(typeof window !== undefined && searchParams.token) {
       console.log('Got token')
       setTimeout(() => {
         console.log('timeout handled');
@@ -15,7 +15,7 @@ export default function Verify({ searchParams }: { searchParams: any }) {
         );
       }, 1000);
     }
-  }, [window, searchParams]);
+  }, [searchParams]);
 
   return <>
     Validating Login...
