81b34916de1dafad51986300d3528ca3964a15be
Guillaume Duhan <31253241+guillaumeduhan@users.noreply.github.com>
2023-06-06 18:25:46 +0100

    commit

--- app/(public)/verify/page.tsx
+++ app/(public)/verify/page.tsx
@@ -1,14 +1,15 @@
 'use client';
+import LoadingApp from '@/components/Loading/App';
+import Typography from '@mui/material/Typography';
 import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
-import Typography from '@mui/material/Typography';
 
 export default function Verify({ searchParams }: { searchParams: any }) {
   const router = useRouter();
 
   useEffect(() => {
-    if(typeof window !== undefined && searchParams.token) {
-      console.log('Got token')
+    if (typeof window !== undefined && searchParams.token) {
+      console.log('Got token');
       setTimeout(() => {
         console.log('timeout handled');
         router.push(
@@ -18,8 +19,10 @@ export default function Verify({ searchParams }: { searchParams: any }) {
     }
   }, [searchParams]);
 
-  return <>
-    Validating Login...
-    <Typography sx={{color: 'white'}}>{searchParams.token}</Typography>
-  </>;
+  return (
+    <>
+      <LoadingApp />
+      <Typography sx={{ color: 'white' }}>{searchParams.token}</Typography>
+    </>
+  );
 }
