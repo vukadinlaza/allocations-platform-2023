2e1ee9b4bdf2b75da5c5ac54daa3af35d20d72c9
Guillaume Duhan <31253241+guillaumeduhan@users.noreply.github.com>
2023-06-22 15:17:15 +0100

    commit

--- components/Deals/Funds/index.tsx
+++ components/Deals/Funds/index.tsx
@@ -1,34 +1,140 @@
 'use client';
-import { headers_tables } from '@/app/(private)/config';
-import PageList from '@/components/Page/List';
-import { PageListData } from '@/types';
-import Card from '@mui/material/Card';
+
+import Button from '@/components/Button';
+import ChipStatus from '@/components/ChipStatus';
+import LoadingList from '@/components/Loading/List';
+import ModalButton from '@/components/Modal/Button';
+import PageHeader from '@/components/Page/Header';
+import { openURL } from '@/components/Table';
+import { useSupabase } from '@/lib/supabase-provider';
+import { orderBy, uniq } from 'lodash';
+import { useEffect, useState } from 'react';
+import DealItem from '../Item';
+import NewDeal from '../New';
+
+const limitation = 25;
 
 export default function Funds() {
-  const data: PageListData = {
-    header: {
-      name: 'Funds',
-      description: 'Manage your funds.',
-      buttons: [
-        {
-          type: 'deal'
-        }
-      ]
-    },
-    table: {
-      element: 'fund',
-      headers: headers_tables.funds,
-      origin: 'private_deals',
-      query: '*',
-      queryType: 'fund',
-      target: 'deals',
-      type: 'fund'
+  const [deals, setDeals] = useState([]);
+  const [filteredDeals, setFilteredDeals] = useState([]);
+  const [selected, setSelected] = useState('All');
+  const [filters, setFilters] = useState(['All']);
+  const [asc, setAsc] = useState<any>('desc');
+  const [order, setOrder] = useState<any>('total_raised_amount');
+  const [limit, setLimit] = useState<number>(limitation);
+  const [loading, setLoading] = useState(true);
+  const { fetchDeals, supabase } = useSupabase();
+
+  const getFunds = async () => {
+    try {
+      setLoading(true);
+      let { data, error } = await fetchDeals('fund');
+
+      if (data) {
+        setDeals(data);
+        setFilteredDeals(data);
+      }
+      setFilters((prev: any) => [
+        ...prev,
+        ...uniq(data.map((x: any) => x.status))
+      ]);
+    } catch (err) {
+      console.log(err);
+    } finally {
+      setLoading(false);
     }
   };
 
+  useEffect(() => {
+    setDeals([]);
+    setFilters(['All']);
+    getFunds();
+  }, []);
+
+  useEffect(() => {
+    setLimit(limitation);
+    if (selected === 'All') return setFilteredDeals(deals);
+    setFilteredDeals(deals.filter((x: any) => x.status === selected));
+  }, [selected]);
+
   return (
-    <Card className="card" variant="outlined">
-      <PageList data={data} />
-    </Card>
+    <div className="container container--card">
+      {loading && <LoadingList />}
+      {!loading && (
+        <div className="w-full">
+          <PageHeader
+            header={{
+              name: 'Funds',
+              description: 'Manage your funds.',
+              length: deals.length
+            }}
+            content={
+              <ModalButton
+                title="Create a new Fund"
+                content={<NewDeal type={'fund'} onCreate={() => {}} />}
+              />
+            }
+          />
+          <div className="flex gap-2 filters">
+            <div className="flex gap-2 mb-6 capitalize">
+              {/* {['created_at', 'total_raised_amount'].map((orderKey: any) => (
+                <div
+                  key={orderKey}
+                  className="cursor-pointer"
+                  onClick={() => setOrder(orderKey)}
+                >
+                  <ChipStatus
+                    selected={order === orderKey}
+                    toSelect={true}
+                    status={orderKey}
+                  />
+                </div>
+              ))} */}
+            </div>
+            <div className="flex gap-2 mb-6 capitalize">
+              {[
+                'All',
+                'draft',
+                'submitted',
+                'onboarding',
+                'closed',
+                'archived'
+              ].map((status) => (
+                <div
+                  key={status}
+                  className="cursor-pointer"
+                  onClick={() => setSelected(status)}
+                >
+                  <ChipStatus
+                    selected={selected === status}
+                    toSelect={true}
+                    status={status}
+                  />
+                </div>
+              ))}
+            </div>
+          </div>
+          <div className="grid gap-2">
+            {orderBy(filteredDeals, [order], [asc])
+              .slice(0, limit)
+              .map((deal: any, index: number) => (
+                <div key={index} onClick={() => openURL(`deals/${deal.id}`)}>
+                  <DealItem deal={deal} details={true} />
+                </div>
+              ))}
+          </div>
+          {filteredDeals.length > limitation && (
+            <div className="mt-6">
+              <Button
+                color="info"
+                small={true}
+                onClick={() => setLimit(limit + limitation)}
+                label={'Load more'}
+              />
+            </div>
+          )}
+        </div>
+      )}
+    </div>
   );
 }
---
+++ components/Deals/Migrations/index.tsx
@@ -0,0 +1,84 @@
+'use client';
+
+import Button from '@/components/Button';
+import LoadingList from '@/components/Loading/List';
+import PageHeader from '@/components/Page/Header';
+import { useSupabase } from '@/lib/supabase-provider';
+import { orderBy } from 'lodash';
+import { useEffect, useState } from 'react';
+import DealItem from '../Item';
+
+const limitation = 25;
+
+export default function Migrations() {
+  const [deals, setDeals] = useState<any>([]);
+  const [limit, setLimit] = useState<number>(limitation);
+  const [loading, setLoading] = useState(true);
+  const { supabase } = useSupabase();
+
+  const getMigrations = async () => {
+    try {
+      setLoading(true);
+      let { data, error } = await supabase
+        .from('private_deals')
+        .select(`*`, { count: 'exact' })
+        .eq('is_migration', true);
+
+      if (data) {
+        setDeals(data);
+      }
+    } catch (err) {
+      console.log(err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    setDeals([]);
+    getMigrations();
+  }, []);
+
+  return (
+    <div className="container container--card">
+      {loading && <LoadingList />}
+      {!loading && (
+        <div className="w-full">
+          <PageHeader
+            header={{
+              name: 'Migrations',
+              description: 'Manage your migrations.',
+              length: deals.length
+            }}
+            content={
+              <div></div>
+              // <ModalButton
+              //   title="Create a new Migration"
+              //   content={<NewDeal type={'spv'} onCreate={() => {}} />}
+              // />
+            }
+          />
+          <div className="grid gap-2">
+            {orderBy(deals, ['created_at'], ['desc'])
+              .slice(0, limit)
+              .map((deal: any, index: number) => (
+                <div key={index}>
+                  <DealItem deal={deal} details={true} />
+                </div>
+              ))}
+          </div>
+          {deals.length > limitation && (
+            <div className="mt-6">
+              <Button
+                color="info"
+                small={true}
+                onClick={() => setLimit(limit + limitation)}
+                label={'Load more'}
+              />
+            </div>
+          )}
+        </div>
+      )}
+    </div>
+  );
+}
